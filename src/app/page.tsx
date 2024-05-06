4d9a4acca276dfd1ece6ec850d310f9d2a1bf484
Martin Winther <martin.winther@protonmail.com>
2024-05-06 13:30:39 +0200

    update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Todo from '@/components/Todo'
 
 export default function Home() {
   return (
-    <main>
+    <main className="dark">
       <div className=" bg-yellow-100 flex justify-center items-center">Hej</div>
       <div className="flex flex-row justify-center items-center space-x-4">
         <Kanban />
