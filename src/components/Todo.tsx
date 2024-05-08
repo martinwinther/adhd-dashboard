b242834a0a457e74239d16c0218312897d98a362
Mordwin1989 <benjamin89@live.dk>
2024-05-08 12:15:47 +0200

    Basic list structure
    We made a basic todo list component and went over react syntax principles

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,14 +1,56 @@
-const Todo = () => {
-  function math(a: number, b: number) {
-    return a + b
+const Todo = ({name}: {name: string}) => {
+
+
+  type ToDoElements = {
+    id: number,
+    todo: string,
+    isComplete: boolean,
+    
   }
+  const tasks: ToDoElements[] = [
+    {
+      id: 1,
+      todo: "Fix the car",
+      isComplete: false
+    },
+    {
+      id: 2,
+      todo: "Clean the oven",
+      isComplete: false
+    },
+    {
+      id: 3,
+      todo: "Go to work",
+      isComplete: false
+    },
+    {
+      id: 4,
+      todo: "Walk the dog",
+      isComplete: false
+    }
+  ];
+
+  const TodoList = ({todolists}: {todolists: ToDoElements[]}) => {
+    return (   
+      <ul>
+        {todolists.map(task => (
+      <li key={task.id}>{task.todo}</li>
+    ))
+  }
+</ul>
+     
+    )
+  }
+
+  
 
   return (
     <div className="bg-red-100 border-2 rounded-lg">
       <h1>This is the todo component</h1>
-      <div>{math(8, 9)}</div>
+      <TodoList todolists={tasks}/>
     </div>
   )
 }
 
 export default Todo
+
