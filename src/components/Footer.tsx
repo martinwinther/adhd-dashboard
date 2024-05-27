d3d34c6b3d4df9b9cd953229bf6d076e77867b23
Mordwin1989 <benjamin89@live.dk>
2024-05-27 12:28:38 +0200

    Weekly checklist structure
    we have added a 7 day structure for the weekly checklist

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
 	return (
 		<div className="flex justify-between items-center px-2 w-full h-12 bg-gray-900 text-white">
-			Footer
+			<h1>Footer</h1>
 		</div>
 	);
 };
--- src/components/WeeklyChecklist.tsx
+++ src/components/WeeklyChecklist.tsx
@@ -3,44 +3,142 @@ const WeeklyChecklist = () => {
 		id: number;
 		todo: string;
 		isComplete: boolean;
+		weekday:
+			| "monday"
+			| "tuesday"
+			| "wednesday"
+			| "thursday"
+			| "friday"
+			| "saturday"
+			| "sunday";
 	};
-	const tasks: ToDoElements[] = [
+
+	const weeklyTasks: ToDoElements[] = [
 		{
 			id: 1,
 			todo: "Fix the car",
 			isComplete: false,
+			weekday: "monday",
 		},
 		{
 			id: 2,
 			todo: "Clean the oven",
 			isComplete: false,
+			weekday: "monday",
 		},
 		{
 			id: 3,
 			todo: "Go to work",
 			isComplete: false,
+			weekday: "tuesday",
 		},
 		{
 			id: 4,
 			todo: "Walk the dog",
 			isComplete: false,
+			weekday: "wednesday",
+		},
+		{
+			id: 5,
+			todo: "Play Super smash bros",
+			isComplete: false,
+			weekday: "friday",
+		},
+		{
+			id: 6,
+			todo: "Throw darts with the boys",
+			isComplete: false,
+			weekday: "friday",
+		},
+		{
+			id: 7,
+			todo: "Workout",
+			isComplete: false,
+			weekday: "tuesday",
+		},
+		{
+			id: 8,
+			todo: "Walk the dog",
+			isComplete: false,
+			weekday: "wednesday",
+		},
+		{
+			id: 9,
+			todo: "Workout",
+			isComplete: false,
+			weekday: "thursday",
+		},
+		{
+			id: 10,
+			todo: "Play Super smash bros",
+			isComplete: false,
+			weekday: "thursday",
+		},
+		{
+			id: 11,
+			todo: "Play Super smash bros",
+			isComplete: false,
+			weekday: "friday",
+		},
+		{
+			id: 12,
+			todo: "Therapy session",
+			isComplete: false,
+			weekday: "friday",
+		},
+		{
+			id: 13,
+			todo: "Hearthstone",
+			isComplete: false,
+			weekday: "saturday",
+		},
+		{
+			id: 14,
+			todo: "Play Super smash bros",
+			isComplete: false,
+			weekday: "sunday",
+		},
+		{
+			id: 15,
+			todo: "Open new packs",
+			isComplete: false,
+			weekday: "sunday",
 		},
 	];
 
 	const TodoList = ({ todolists }: { todolists: ToDoElements[] }) => {
 		return (
-			<ul>
-				{todolists.map((task) => (
-					<li key={task.id}>{task.todo}</li>
+			<div className="flex flex-col md:flex-row justify-between space-y-2 md:space-x-2 md:space-y-0 items-stretch">
+				{[
+					"monday",
+					"tuesday",
+					"wednesday",
+					"thursday",
+					"friday",
+					"saturday",
+					"sunday",
+				].map((day) => (
+					<div key={day} className="flex-1 flex flex-col">
+						<div className="text-lg font-bold">{day}</div>
+						<ul className="flex-1 border-2 border-black p-2 flex flex-col">
+							{todolists
+								.filter((task) => task.weekday === day)
+								.map((task) => (
+									<li key={task.id} className="mt-1">
+										{task.todo}
+									</li>
+								))}
+						</ul>
+					</div>
 				))}
-			</ul>
+			</div>
 		);
 	};
 
 	return (
 		<div className="bg-red-100 border-2 rounded-lg p-2">
 			<h1>This is the WeeklyChecklist component</h1>
-			<TodoList todolists={tasks} />
+			<TodoList todolists={weeklyTasks} />
 		</div>
 	);
 };
