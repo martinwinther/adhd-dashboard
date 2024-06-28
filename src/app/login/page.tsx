28809c437bed9659593d9ce67a5feff739a78e37
Martin Winther <martin.winther@protonmail.com>
2024-06-28 13:38:36 +0200

    add basic auth.js auth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@/components/sign-in";
+
+export default function Home() {
+	return (
+		<main className="flex flex-col justify-between h-screen">
+			<SignIn />
+		</main>
+	);
+}
---
+++ src/auth.ts
@@ -0,0 +1,12 @@
+import { Pool } from "@neondatabase/serverless";
+import NextAuth from "next-auth";
+import PostgresAdapter from "@auth/pg-adapter";
+import github from "next-auth/providers/github";
+
+export const { handlers, signIn, signOut, auth } = NextAuth(() => {
+	const pool = new Pool({ connectionString: process.env.POSTGRES_URL });
+	return {
+		adapter: PostgresAdapter(pool),
+		providers: [github],
+	};
+});
