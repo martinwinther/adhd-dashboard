cd62854f882a680f867d100db39b6fa009f2973c
Martin Winther <martin.winther@protonmail.com>
2024-08-19 13:02:05 +0200

    rm imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import Navigation from "@/components/Navigation";
-import Kanban from "@/app/dashboard/components/Kanban";
-import WeeklyChecklist from "@/app/dashboard/components/WeeklyChecklist";
-import DailyCheckList from "@/app/dashboard/components/DailyChecklist";
-import Footer from "@/components/Footer";
-import TodaysList from "@/app/dashboard/components/TodaysList";
-import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import Link from "next/link";
 
